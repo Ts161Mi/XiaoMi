8aee1d6bc74fa21dc50ab195ffa0b59d4d2530b5
976505152@qq.com <976505152@qq.com>
2018-07-05 14:28:10 +0800

    小米商城

---
+++ xiaomi/js/miJs.js
@@ -0,0 +1,9 @@
+
+// function Shover() {
+//     $("#shopping a").mouseover(function () {
+//         $(this).children().css({"background-color":" white","color":"rgb(255,103,0)"});
+//     }).mouseout(function () {
+//         $(this).children().css({"background-color":" #b0b0b0","color":"rgb(66,66,66)"});
+//     });
+// }
+
