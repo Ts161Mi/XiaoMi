c991e935cab0892ce18e471b412d5d1045bd5228
976505152@qq.com <976505152@qq.com>
2018-07-07 06:36:35 +0800

    小米首页更新

--- xiaomi/js/miJs.js
+++ xiaomi/js/miJs.js
@@ -1,9 +1,24 @@
 
-// function Shover() {
-//     $("#shopping a").mouseover(function () {
-//         $(this).children().css({"background-color":" white","color":"rgb(255,103,0)"});
-//     }).mouseout(function () {
-//         $(this).children().css({"background-color":" #b0b0b0","color":"rgb(66,66,66)"});
-//     });
-// }
+
+$(function () {
+    $("#shopping").mouseover(function () {
+        $("#commodity").show();
+        var num=$("#commodity").children("ul").size();
+        if(num>0){
+
+        }else{
+            $("#commodity .loading").text("购物车中还没有商品，赶紧选购吧！");
+        }
+    }).mouseout(function () {
+        $("#commodity").hide();
+    })
+    $("#search").focus(function () {
+        $(".search-hot-words").hide();
+        $(".header-search .keyword-list").css("display","block").siblings().css("border","1px solid #ff6700");
+    }).blur(function () {
+        $(".search-hot-words").show();
+        $(".header-search .keyword-list").css("display","none").siblings().css("border","1px solid #e0e0e0");
+        $(".header-search .search-hot-words").css("border","none");
+    })
+})
 
